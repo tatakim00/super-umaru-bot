50f9cc7272a6821ecfcd3b923386e4e8e42e0a15
tatakim00 <56082152+tatakim00@users.noreply.github.com>
2020-09-07 14:55:12 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,10 +1,9 @@
 const Discord = require('discord.js')
 const bot = new Discord.Client();
 
-const token = 'NzUyNTAzMzU2MjM2NDk2OTU2.X1YlXA.ieu0K5TEKwCLTKqoaoWscAWtm9c';
 
-bot.on('ready', () =>{
+client.on('ready', () =>{
     console.log('Im here Daddy!')
 });
 
-bot.login(process.env.BOT_TOKEN);
+client.login(process.env.BOT_TOKEN);
