ec09834921391882476bd4b9e9421708c8c8c982
tatakim00 <56082152+tatakim00@users.noreply.github.com>
2020-09-07 15:53:18 +0200

    Delete index.js

--- index.js
+++
@@ -1,9 +0,0 @@
-const Discord = require('discord.js')
-const bot = new Discord.Client();
-
-
-client.on('ready', () =>{
-    console.log('Im here Daddy!')
-});
-
-client.login(process.env.BOT_TOKEN);
